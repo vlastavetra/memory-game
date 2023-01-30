f4eede33436fd42f4474302655d17e669f34c9ee
Vlasta Vetra <vlastavetra@gmail.com>
2023-01-30 17:22:15 +0200

    created api methods for score getting

--- server/controllers/scoresController.js
+++ server/controllers/scoresController.js
@@ -1,4 +1,10 @@
-const { addScoreModel } = require("../models/scoresModel");
+const {
+  addScoreModel,
+  getScoresModel,
+  getScoresByUserNickname,
+  getScoresByUserNicknameLast,
+  getScoresByUserNicknameHighest,
+} = require("../models/scoresModel");
 
 const addScore = async (req, res) => {
   try {
@@ -9,6 +15,49 @@ const addScore = async (req, res) => {
   }
 };
 
+const getAllScores = async (req, res) => {
+  try {
+    const allData = await getScoresModel();
+    res.status(200).send(allData);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const getUserScores = async (req, res) => {
+  const { nickname } = req.body;
+  try {
+    const data = await getScoresByUserNickname(nickname);
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const getUserLastScores = async (req, res) => {
+  const { nickname } = req.body;
+  try {
+    const data = await getScoresByUserNicknameLast(nickname);
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const getUserHighestScores = async (req, res) => {
+  const { nickname } = req.body;
+  try {
+    const data = await getScoresByUserNicknameHighest(nickname);
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
 module.exports = {
   addScore,
+  getAllScores,
+  getUserScores,
+  getUserLastScores,
+  getUserHighestScores,
 };
