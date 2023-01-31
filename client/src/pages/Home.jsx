ac7d163a6bc2dd529d61cc9c29134ec8f1b51865
salomemedd <salome.medioni.pro@gmail.com>
2023-01-31 18:34:36 +0200

    get user infos

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 
+
+
 const Home = () => {
+  
+    
+   
     return ( <>
     <div className='title-home'>MEMO MEME</div>
+    <div>Welcome </div>
     <div>Some text..... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
     <button className='main-button'>PLAY</button>
     </> );
