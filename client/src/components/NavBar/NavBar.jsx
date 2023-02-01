cbe13eb9beb8ca82f31c766b7e24646f88f1e31f
kgoodluck <k-goodluck@yandex.ru>
2023-02-01 05:16:49 +0200

    woow changed nav links to resemble one-page app

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import './NavBar.css';
 import ModalLoginSign from '../../modal/ModalLoginSignup';
+import sound1 from "../../assets/sounds/select-menu-47560.mp3";
+import { NavLink } from "react-router-dom";
 
 //bootstrap
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
 const NavBar = () => {
+
+  const handleMouseDown = () => {
+    const clickAudio = new Audio(sound1);
+    clickAudio.play();
+  }
+
   return (
     <>
       <Navbar className="nav-bar">
         <Container fluid>
-          <Nav className="me-auto my-2 my-lg-0">
+          <Nav className="me-auto my-2 my-lg-0" onMouseDown={handleMouseDown}>
             <ModalLoginSign />
-            <Nav.Link href="/" className="link">
+            <NavLink to="/" className="link nav-link">
               Home
-            </Nav.Link>
-            <Nav.Link href="/score" className="link">
+            </NavLink>
+            <NavLink to="/score" className="link nav-link">
               Score
-            </Nav.Link>
-            <Nav.Link href="/game" className="link">
+            </NavLink>
+            <NavLink to="/game" className="link nav-link">
               Game
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Container>
       </Navbar>
