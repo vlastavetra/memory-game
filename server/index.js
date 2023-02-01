782314ace8dabb775c85e9c1fa6e2ce5d32bd4b4
Vlasta Vetra <vlastavetra@gmail.com>
2023-02-01 19:23:05 +0300

    added headers for cors

--- server/index.js
+++ server/index.js
@@ -20,6 +20,15 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors({ origin: ['http://localhost:3000', 'https://meme-memory-game.vercel.app/'], credentials: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 app.use('/user', userRoutes);
 
