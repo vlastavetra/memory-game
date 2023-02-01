e5d9d624317080a8a214e04765e77143b2c30db8
salomemedd <salome.medioni.pro@gmail.com>
2023-02-01 12:06:28 +0200

    fix

--- server/middlewares/scoresMiddleware.js
+++ server/middlewares/scoresMiddleware.js
@@ -3,7 +3,7 @@ const { getUserById } = require("../models/userModel");
 const getUserNickname = async (req, res, next) => {
   const { id } = req.params;
   const user = await getUserById(id);
-  req.body.nickname = user.username;
+  req.body.username = user.username;
 
   next();
 };
--- server/middlewares/validateUserId.js
+++ server/middlewares/validateUserId.js
@@ -2,15 +2,15 @@ const User = require("../models/userSchema");
 
 const validateUserId = async (req, res, next) => {
   try {
-    console.log("here" , req.params.id);
+    
     const user = await User.findById(req.params.id);
-    console.log("user" , user);
+
     if (!user) {
       return res.status(404).json({ message: "Invalid user ID" });
     }
     next();
   } catch (error) {
-    console.log(error);
+   
     return res.status(500).json({ message:  error.message });
   }
 };
