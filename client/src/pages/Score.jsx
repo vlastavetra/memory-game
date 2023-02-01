d0986e467c913055b5733819f84e2edda79c3f97
Vlasta Vetra <vlastavetra@gmail.com>
2023-02-01 12:18:24 +0300

    added score tables

--- client/src/pages/Score.jsx
+++ client/src/pages/Score.jsx
@@ -1,21 +1,24 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import dayjs from "dayjs";
+import "./Score.css";
 
 //todo: token, loader
 
 const Score = () => {
   const [allUsersScore, setAllUsersScore] = useState([]);
-  const [userHistoryScore, setUserHistoryScore] = useState({});
-  const [userLastScore, setUserLastScore] = useState({});
-  const [userHighScore, setUserHighScore] = useState({});
+  const [userHistoryScore, setUserHistoryScore] = useState([]);
+  const [userLastScore, setUserLastScore] = useState("");
+  const [userHighScore, setUserHighScore] = useState("");
+  const [userId, setUserId] = useState(localStorage.getItem("userId"));
+  const [token, setToken] = useState("");
 
   const getAllUsersData = async () => {
     try {
       const res = await axios.get(`http://127.0.0.1:8080/scores`, {
-        //headers: { authorization: `Bearer ${token}` },
+        headers: { authorization: `Bearer ${token}` },
       });
       setAllUsersScore(res.data);
-      console.log(res.data)
       //showLoader(false);
     } catch (err) {
       console.log(err);
@@ -24,11 +27,10 @@ const Score = () => {
 
   const getUserHistoryScore = async () => {
     try {
-      const res = await axios.get(`http://127.0.0.1:8080/scores/63d7d933c6059f6ae743b2fb`, {
-        //headers: { authorization: `Bearer ${token}` },
+      const res = await axios.get(`http://127.0.0.1:8080/scores/${userId}`, {
+        headers: { authorization: `Bearer ${token}` },
       });
       setUserHistoryScore(res.data);
-      console.log(res.data)
       //showLoader(false);
     } catch (err) {
       console.log(err);
@@ -37,11 +39,13 @@ const Score = () => {
 
   const getUserLastScore = async () => {
     try {
-      const res = await axios.get(`http://127.0.0.1:8080/scores/last/63d7d933c6059f6ae743b2fb`, {
-        //headers: { authorization: `Bearer ${token}` },
-      });
-      setUserLastScore(res.data);
-      console.log(res.data)
+      const res = await axios.get(
+        `http://127.0.0.1:8080/scores/last/${userId}`,
+        {
+          headers: { authorization: `Bearer ${token}` },
+        }
+      );
+      setUserLastScore(res.data[0].score);
       //showLoader(false);
     } catch (err) {
       console.log(err);
@@ -50,11 +54,13 @@ const Score = () => {
 
   const getUserHighScore = async () => {
     try {
-      const res = await axios.get(`http://127.0.0.1:8080/scores/high/63d7d933c6059f6ae743b2fb`, {
-        //headers: { authorization: `Bearer ${token}` },
-      });
-      setUserHighScore(res.data);
-      console.log(res.data)
+      const res = await axios.get(
+        `http://127.0.0.1:8080/scores/high/${userId}`,
+        {
+          headers: { authorization: `Bearer ${token}` },
+        }
+      );
+      setUserHighScore(res.data[0].score);
       //showLoader(false);
     } catch (err) {
       console.log(err);
@@ -62,6 +68,8 @@ const Score = () => {
   };
 
   useEffect(() => {
+    setUserId(localStorage.getItem("userId"));
+    setToken(localStorage.getItem("token"));
     getAllUsersData();
     getUserHistoryScore();
     getUserLastScore();
@@ -71,26 +79,48 @@ const Score = () => {
 
   return (
     <main className="main-usermatch-container">
-        <h1 className="usermatch-title">Scores</h1>
-        <table className="usermatch-table">
-          <thead className="usermatch-table-head">
-            <tr className="usermatch-table-head-row">
-              <th className="usermatch-table-head-column">User</th>
-              <th className="usermatch-table-head-column">Score</th>
-              <th className="usermatch-table-head-column">Date</th>
+      <h1 className="usermatch-title">Scores</h1>
+      <div className="user-score-container">
+        <div className="user-score"><span>Your last score: </span><span>{userLastScore}</span></div>
+        <div className="user-score"><span>Your high score: </span><span>{userHighScore}</span></div>
+      </div>
+      <div className="tables-container">
+      <table className="usermatch-table">
+        <thead className="usermatch-table-head">
+          <tr className="usermatch-table-head-row">
+            <th className="usermatch-table-head-column">Score</th>
+            <th className="usermatch-table-head-column">Date</th>
+          </tr>
+        </thead>
+        <tbody className="usermatch-table-body">
+          {userHistoryScore.map((obj) => (
+            <tr key={obj._id} className="usermatch-table-body-row">
+              <td className="usermatch-table-body-column">{obj.score}</td>
+              <td className="usermatch-table-body-column">{dayjs(obj.date).format('HH:mm YYYY-MM-DD')}</td>
             </tr>
-          </thead>
-          <tbody className="usermatch-table-body">
-            {allUsersScore.map((obj) => (
-              <tr key={obj._id} className="usermatch-table-body-row">
-                <td className="usermatch-table-body-column">{obj.nickname}</td>
-                <td className="usermatch-table-body-column">{obj.score}</td>
-                <td className="usermatch-table-body-column">{obj.date}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </main>
+          ))}
+        </tbody>
+      </table>
+      <table className="usermatch-table">
+        <thead className="usermatch-table-head">
+          <tr className="usermatch-table-head-row">
+            <th className="usermatch-table-head-column">User</th>
+            <th className="usermatch-table-head-column">Score</th>
+            <th className="usermatch-table-head-column">Date</th>
+          </tr>
+        </thead>
+        <tbody className="usermatch-table-body">
+          {allUsersScore.map((obj) => (
+            <tr key={obj._id} className="usermatch-table-body-row">
+              <td className="usermatch-table-body-column">{obj.nickname}</td>
+              <td className="usermatch-table-body-column">{obj.score}</td>
+              <td className="usermatch-table-body-column">{dayjs(obj.date).format('HH:mm YYYY-MM-DD')}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    </main>
   );
 };
 
