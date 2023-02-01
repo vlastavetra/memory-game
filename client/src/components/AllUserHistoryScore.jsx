552cfd911f5dd8d39198f936eabf406908750722
Vlasta Vetra <vlastavetra@gmail.com>
2023-02-01 13:13:47 +0300

    score page refactoring

---
+++ client/src/components/AllUserHistoryScore.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import dayjs from "dayjs";
+import "./UserHistoryScore.css";
+
+const AllUserHistoryScore = () => {
+  const [allUsersScore, setAllUsersScore] = useState([]);
+  const [token, setToken] = useState("");
+
+  const getAllUsersData = async () => {
+    try {
+      const res = await axios.get(`http://127.0.0.1:8080/scores`, {
+        headers: { authorization: `Bearer ${token}` },
+      });
+      setAllUsersScore(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    setToken(localStorage.getItem("token"));
+    getAllUsersData();
+  }, []);
+
+  return (
+    <div className="score-table-container">
+      <h2 className="score-title-h2">All users scores history</h2>
+      <table className="score-table">
+        <thead className="score-table-head">
+          <tr className="score-table-head-row">
+            <th className="score-table-head-column">User</th>
+            <th className="score-table-head-column">Score</th>
+            <th className="score-table-head-column">Date</th>
+          </tr>
+        </thead>
+        <tbody className="score-table-body">
+          {allUsersScore.map((obj) => (
+            <tr key={obj._id} className="score-table-body-row">
+              <td className="score-table-body-column">{obj.nickname}</td>
+              <td className="score-table-body-column">{obj.score}</td>
+              <td className="score-table-body-column">
+                {dayjs(obj.date).format("HH:mm YYYY-MM-DD")}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default AllUserHistoryScore;
---
+++ client/src/components/UserScore.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./UserScore.css";
+
+const UserScore = () => {
+  const [userLastScore, setUserLastScore] = useState("");
+  const [userHighScore, setUserHighScore] = useState("");
+  const [userId, setUserId] = useState(localStorage.getItem("userId"));
+  const [token, setToken] = useState("");
+
+  const getUserLastScore = async () => {
+    try {
+      const res = await axios.get(
+        `http://127.0.0.1:8080/scores/last/${userId}`,
+        {
+          headers: { authorization: `Bearer ${token}` },
+        }
+      );
+      setUserLastScore(res.data[0].score);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getUserHighScore = async () => {
+    try {
+      const res = await axios.get(
+        `http://127.0.0.1:8080/scores/high/${userId}`,
+        {
+          headers: { authorization: `Bearer ${token}` },
+        }
+      );
+      setUserHighScore(res.data[0].score);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    setUserId(localStorage.getItem("userId"));
+    setToken(localStorage.getItem("token"));
+    getUserLastScore();
+    getUserHighScore();
+  }, []);
+
+  return (
+    <div className="user-score-container">
+      <div className="user-score">
+        <span>Your last score: </span>
+        <span>{userLastScore}</span>
+      </div>
+      <div className="user-score">
+        <span>Your high score: </span>
+        <span>{userHighScore}</span>
+      </div>
+    </div>
+  );
+};
+
+export default UserScore;
