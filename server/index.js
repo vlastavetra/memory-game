faccc299a6c9e98d96213c77eb823b4c11f1fccd
Vlasta Vetra <vlastavetra@gmail.com>
2023-02-01 19:34:06 +0300

    fixes for cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-var config = require('./config.js');
+var config = require("./config.js");
 var ALLOWED_ORIGIN = config.ALLOWED_ORIGIN;
 var MONGODB_URI = config.MONGODB_URI;
-const onConnection = require('./socket_io/onConnection.js');
-const { getFilePath } = require('./utils/file.js');
-const onError = require('./utils/onError.js');
-const upload = require('./utils/upload.js');
+const onConnection = require("./socket_io/onConnection.js");
+const { getFilePath } = require("./utils/file.js");
+const onError = require("./utils/onError.js");
+const upload = require("./utils/upload.js");
 const dotenv = require("dotenv");
 const express = require("express");
 const { createServer } = require("http");
@@ -19,7 +19,20 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:3000', 'https://meme-memory-game.vercel.app/', 'https://memory-game-server.vercel.app'], credentials: true }));
+app.options("*", cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://meme-memory-game.vercel.app/",
+      "https://memory-game-server.vercel.app",
+    ],
+    credentials: true,
+    preflightContinue: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS'],
+    origin: true
+  })
+);
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
@@ -30,10 +43,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/user', userRoutes);
+app.use("/user", userRoutes);
 
 app.use("/scores", scoresRoute);
-
 
 app.use("*", (req, res) => {
   res.status(404).send({ message: "Oops page not found" });
@@ -43,42 +55,42 @@ app.use((err, req, res, next) => {
   res.status(err.statusCode).send(err.message);
 });
 
-app.use('/upload', upload.single('file'), (req, res) => {
-  if (!req.file) return res.sendStatus(400)
+app.use("/upload", upload.single("file"), (req, res) => {
+  if (!req.file) return res.sendStatus(400);
 
   const relativeFilePath = req.file.path
-    .replace(/\\/g, '/')
-    .split('server/files')[1]
+    .replace(/\\/g, "/")
+    .split("server/files")[1];
 
-  res.status(201).json(relativeFilePath)
-})
+  res.status(201).json(relativeFilePath);
+});
 
-app.use('/files', (req, res) => {
-  const filePath = getFilePath(req.url)
+app.use("/files", (req, res) => {
+  const filePath = getFilePath(req.url);
 
-  res.status(200).sendFile(filePath)
-})
+  res.status(200).sendFile(filePath);
+});
 
-app.use(onError)
+app.use(onError);
 
-const server = createServer(app)
+const server = createServer(app);
 
 const io = new Server(server, {
   cors: ALLOWED_ORIGIN,
-  serveClient: false
-})
+  serveClient: false,
+});
 
-io.on('connection', (socket) => {
-  onConnection(io, socket)
-})
+io.on("connection", (socket) => {
+  onConnection(io, socket);
+});
 
 async function init() {
   const connection = await mongoose
     .set("strictQuery", true)
-    .connect(process.env.MONGO_URI, { 
+    .connect(process.env.MONGO_URI, {
       dbName: "memoryGameDb",
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
   if (connection) {
     console.log("Connected to DB");
